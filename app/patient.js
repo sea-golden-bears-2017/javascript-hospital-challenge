89295ec9df867f806c005bcbecfe74f11ae38df6
Taylor Svendsen <taysvendsen@gmail.com>
2017-10-24 09:59:17 -0700

    initial commit

---
+++ app/patient.js
@@ -0,0 +1,5 @@
+class Patient {
+  constructor(name) {
+    this.name = name;
+  }
+}
--- app/physician.js
+++ app/physician.js
@@ -4,6 +4,23 @@ class Physician {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // This pattern of adding a class to global is **NOT** standard and not best-practice
 // The standard way of doing so is ES2015 modules, using the export keyword
 // Using exports will be covered in a later challenge
